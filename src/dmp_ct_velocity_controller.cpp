38ad76522815960b969c4dbec861194c6ce562f8
Konstantin Ritt <k.ritt@tum.de>
2019-04-11 20:57:08 +0200

    reserve space for vectors

--- src/dmp_ct_velocity_controller.cpp
+++ src/dmp_ct_velocity_controller.cpp
@@ -38,6 +38,8 @@ bool DmpCtVelocityController::init(hardware_interface::RobotHW* robot_hardware,
 
     initROSCommunication();
 
+    msgBatch.samples.reserve(1/tau/dt/30);
+
     return true;
 }
 
@@ -226,6 +228,9 @@ bool DmpCtVelocityController::loadDmpData(int &dofs, int &nBFs, double &dt, std:
     std::vector<double> goalvTemp(goal.begin(), goal.end());
     y0v = y0vTemp;
     goalv = goalvTemp;
+
+    refDmpTraj.reserve(int(timeSpan/dt));
+    refDmpVel.reserve(int(timeSpan/dt));
 
     return true;
 }
