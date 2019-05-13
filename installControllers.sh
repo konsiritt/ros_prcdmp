44f73a7f811d640a93c27f4c6af20a8fcaebba76
Konstantin Ritt <k.ritt@tum.de>
2019-05-13 16:21:40 +0200

    install script no longer overwrites locally different files (devel from catkin)

--- installControllers.sh
+++ installControllers.sh
@@ -6,7 +6,7 @@ cd ${ROOT}
 
 ### make ###
 cd ${ROOT}/../../..
-catkin_make
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFranka_DIR:PATH=../../libfranka/build
 
 ### push all build files of the project ###
 
@@ -14,5 +14,9 @@ cd ${ROOT}/../../../..
 
 if [ ! -z "$1" ];
 then
-rsync -r franka_ros_catkin franka_adm@$IP:~/
+rsync -r franka_ros_catkin/src franka_adm@$IP:~/franka_ros_catkin/
+rsync -r franka_ros_catkin/build franka_adm@$IP:~/franka_ros_catkin/
+rsync -r franka_ros_catkin/devel/lib franka_adm@$IP:~/franka_ros_catkin/devel/
+rsync -r franka_ros_catkin/devel/share franka_adm@$IP:~/franka_ros_catkin/devel/ 
+rsync -r franka_ros_catkin/devel/include franka_adm@$IP:~/franka_ros_catkin/devel/ 
 fi
